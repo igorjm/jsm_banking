fd4b3e7aae0698caf1d08ba797d63101b0024cab
Igor Melo <igorjmelo4@gmail.com>
2024-08-23 08:13:43 -0300

    feat: header box

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,22 @@
+import HeaderBox from "@/components/HeaderBox";
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  const loggedIn = { firstName: "Igor" };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+        </header>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
---
+++ components/HeaderBox.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HeaderBox = ({
+  type = "title",
+  title,
+  subtext,
+  user,
+}: HeaderBoxProps) => {
+  return <div>HeaderBox</div>;
+};
+
+export default HeaderBox;
