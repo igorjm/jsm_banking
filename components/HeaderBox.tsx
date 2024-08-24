4769f18c37a4f20369d33c921162335f56b25c45
Igor Melo <igorjmelo4@gmail.com>
2024-08-24 12:40:50 -0300

    feat: header box component

--- components/HeaderBox.tsx
+++ components/HeaderBox.tsx
@@ -6,7 +6,17 @@ const HeaderBox = ({
   subtext,
   user,
 }: HeaderBoxProps) => {
-  return <div>HeaderBox</div>;
+  return (
+    <div className="header-box">
+      <h1 className="header-box-title">
+        {title}
+        {type === "greeting" && (
+          <span className="text-bankGradient">&nbsp;{user}</span>
+        )}
+      </h1>
+      <p className="header-box-subtext">{subtext}</p>
+    </div>
+  );
 };
 
 export default HeaderBox;
