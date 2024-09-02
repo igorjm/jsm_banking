d2274d23228a30d029e0ab268c94c6146011d48c
Igor Melo <igorjmelo4@gmail.com>
2024-09-02 17:36:20 -0300

    feat: new doughnut component

--- components/AnimatedCounter.tsx
+++ components/AnimatedCounter.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import CountUp from "react-countup/build/CountUp";
+import CountUp from "react-countup";
 
 const AnimatedCounter = ({ amount }: { amount: number }) => {
   return (
-    <div>
-      <CountUp duration={4} decimals={2} decimal="," prefix="$" end={amount} />
+    <div className="w-full">
+      <CountUp decimals={2} decimal="," prefix="$" end={amount} />
     </div>
   );
 };
